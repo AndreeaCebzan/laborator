201a2f68de22c0bf690f5f730cd6ab6fe928b5b4
AndreeaCebzan <81323520+AndreeaCebzan@users.noreply.github.com>
2021-11-11 13:12:41 +0200

    L05

---
+++ L05/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace L05
+{
+    class Program
+    { 
+           static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var count=0;
+             while(true)
+             {
+                 Console.WriteLine("Cycle no." +count.ToString());
+                 Console.WriteLine("Date:"+DateTime.Now.ToLocalTime());
+                 count++;
+                 Thread.Sleep(2000);
+             }
+        }
+    }
+}
